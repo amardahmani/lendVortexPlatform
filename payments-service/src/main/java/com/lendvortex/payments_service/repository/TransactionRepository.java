f82dab1b6b4ea4f2e43d8515a558909aa4458140
Amar Mahdi DAHMANI <amardahmani111@gmail.com>
2024-10-06 05:16:38 +0100

    resetting payment-service

--- payments-service/src/main/java/com/lendvortex/payments_service/repository/TransactionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lendvortex.payments_service.repository;
-
-import com.lendvortex.payments_service.entity.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionRepository extends JpaRepository<Transaction,Long> {
-}
