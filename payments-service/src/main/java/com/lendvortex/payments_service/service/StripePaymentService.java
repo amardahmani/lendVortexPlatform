c5cd1b987eebecad409d6eab8fa67cc2e9d2ac0d
Amar Mahdi DAHMANI <amardahmani111@gmail.com>
2024-10-08 14:40:03 +0100

    payment not working restarting again

--- payments-service/src/main/java/com/lendvortex/payments_service/service/StripePaymentService.java
+++ payments-service/src/main/java/com/lendvortex/payments_service/service/StripePaymentService.java
@@ -1,20 +1,10 @@
 package com.lendvortex.payments_service.service;
 
-import com.lendvortex.payments_service.dto.request.LoanDisbursementRequest;
-import com.lendvortex.payments_service.dto.response.DisbursementResponse;
-import com.lendvortex.payments_service.dto.response.PaymentResponse;
 import com.stripe.exception.StripeException;
 import com.stripe.model.PaymentIntent;
 
 import java.math.BigDecimal;
 
 public interface StripePaymentService {
-    PaymentIntent createPayment(Long userId, BigDecimal amount,String currency) throws StripeException;
-    void syncCardWithStripe();
-
-    PaymentResponse processPyament(String cardId,BigDecimal amount) throws StripeException;
-    String createNewStripeCustomer(Long userId) throws StripeException;
-
-    DisbursementResponse distributeLoan(LoanDisbursementRequest loanDisbursementRequest) throws StripeException;
 
 }
